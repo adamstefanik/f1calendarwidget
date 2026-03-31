8bf58e186f78ea5f0a95424655826091e5b3311f
Adam Samuel Štefánik <adamm.stefanik11@gmail.com>
2026-03-31 14:10:18 +0200

    Add NSViewRepresentable variant for macOS Lottie support

--- F1WidgetExtension/F1CalendarWidget/Views/LottieView.swift
+++ F1WidgetExtension/F1CalendarWidget/Views/LottieView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import Lottie
 
+#if os(iOS)
 struct LottieView: UIViewRepresentable {
     let fileName: String
     var loopMode: LottieLoopMode = .loop
@@ -28,3 +29,31 @@ struct LottieView: UIViewRepresentable {
 
     func updateUIView(_ uiView: UIView, context: Context) {}
 }
+#elseif os(macOS)
+struct LottieView: NSViewRepresentable {
+    let fileName: String
+    var loopMode: LottieLoopMode = .loop
+
+    func makeNSView(context: Context) -> NSView {
+        let container = NSView()
+
+        let animationView = LottieAnimationView(name: fileName)
+        animationView.loopMode = loopMode
+        animationView.contentMode = .scaleAspectFit
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.play()
+
+        container.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            animationView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            animationView.topAnchor.constraint(equalTo: container.topAnchor),
+            animationView.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+        ])
+
+        return container
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {}
+}
+#endif
